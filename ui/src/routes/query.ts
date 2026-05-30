4bf42eb8aa3c7b8254227c96527025ca16363b1f
Traines <git@traines.eu>
2026-05-30 20:54:14 +0000

    fix for stopids with url special chars

--- ui/src/routes/query.ts
+++ ui/src/routes/query.ts
@@ -2,7 +2,7 @@ const knownErrors = [400, 500, 502, 503, 504];
 
 function handleHttpErrors(response: Response) {
     if (!response.ok) {
-        const errorMsgId = 'error_http_'+ (knownErrors.indexOf(response.status) != -1 ? response.status : 'unknown');
+        const errorMsgId = 'error_http_' + (knownErrors.indexOf(response.status) != -1 ? response.status : 'unknown');
         throw new Error(errorMsgId);
     }
     return response.json();
@@ -10,26 +10,26 @@ function handleHttpErrors(response: Response) {
 
 function queryString(q: any): string {
     return Object.keys(q)
-    .map(
-        k => Array.isArray(q[k]) 
-        ? q[k].map((v: any) => ({k: k, v: v}))
-        : [{k: k, v: q[k]}]
-    )
-    .flat()
-    .map(o => o.k+'='+(o.v ? o.v : ''))
-    .join('&');
+        .map(
+            k => Array.isArray(q[k])
+                ? q[k].map((v: any) => ({ k: k, v: v }))
+                : [{ k: k, v: q[k] }]
+        )
+        .flat()
+        .map(o => o.k + '=' + (o.v ? o.v : ''))
+        .join('&');
 }
 
 function optionsQueryString(query: any, datetime?: string): any {
     const q = {
-        from: query.from.id,
-        to: query.to.id,
-        vias: query.vias.map((v: any) => v?.id),
+        from: encodeURIComponent(query.from.id),
+        to: encodeURIComponent(query.to.id),
+        vias: query.vias.map((v: any) => v?.id ? encodeURIComponent(v?.id) : undefined),
         datetime: query.datetime || datetime,
         regionly: query.regionly,
         system: query.system
     };
-    return queryString(q);        
+    return queryString(q);
 }
 
 export {
